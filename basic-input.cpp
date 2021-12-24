34712eb7beb5e1b3374546e2746c5c15641916f3
akashtechie <76900147+akashtechie@users.noreply.github.com>
2021-12-24 14:05:37 +0530

    Add files via upload

---
+++ basic-input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout << "Type your name :";
+    cin >> name;
+    cout << "Hello World";
+    cout << "\nMy name is " << name;
+    return 0;
+}
